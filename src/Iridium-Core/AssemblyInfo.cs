ee906fccdca43662c97c30724b762b176b6c9e91
Philippe Leybaert <philippe@activa.be>
2017-08-24 15:02:09 -0500

    Migration to .NET Standard 1.0

--- src/Iridium-Core/AssemblyInfo.cs
+++ src/Iridium-Core/AssemblyInfo.cs
@@ -1,3 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Iridium-Core.Test")]
+
+[assembly: AssemblyVersion("1.4.0")]
